27ebdb2326385e60736a2921774ff08b2ebde6d0
jorand <jbjorand2@gmail.com>
2018-01-18 11:32:04 +0100

    update view AR

--- app/src/main/java/com/ensicaen/ecole/ludistreet/RA/ARView.java
+++ app/src/main/java/com/ensicaen/ecole/ludistreet/RA/ARView.java
@@ -17,65 +17,19 @@ import worldData.World;
 public class ARView {
     private WallModel _model;
     private Color _color = new Color(0.0f, 0.0f, 0.0f, 0.8f);
-    private DefaultARSetup _setup;
-    private WallSetup _set;
+    private WallSetup _setup;
 
     public ARView(WallModel model){
         _model = model;
-
-        _set = new WallSetup(_color, new Vec(0,2,-50), model);
-
-
-        //initSetup(new Vec(0,2,-50));
+        _setup = new WallSetup(_color, new Vec(0,2,-50), model);
     }
 
     public void setColor(Color c) {
         _color = c;
     }
 
-
-    public void initSetup(final Vec positionUser) {
-         _setup = new DefaultARSetup() {
-            @Override
-            public void addObjectsTo(GL1Renderer renderer, World world, GLFactory objectFactory) {
-                ArrayList<Shape> formes = new ArrayList();
-                int x = _model.getResX();
-                int y = _model.getResY();
-
-                for (int i = 0; i < x; i++) {
-                    for (int j = 0; j < y; j++) {
-                        final Shape square = objectFactory.newSquare(_model.getPixel(i,j));
-                        final int finalI = i;
-                        final int finalJ = j;
-                        square.setOnClickCommand(new Command() {
-                            @Override
-                            public boolean execute() {
-                                square.setColor(_color);
-                                _model.setColorPixel(finalI, finalJ, _color);
-                                return true;
-                            }
-                        });
-                        formes.add(square);
-                    }
-                }
-
-                for(int i = 0; i < x; i++){
-                    for (int j = 0; j < y; j++) {
-                        formes.get(i*y + j).setRotation(new Vec(0, 90, 0));
-                        formes.get(i*y + j).setPosition(new Vec(0, 2 * j, 2*i));
-                        world.add(formes.get(i*y + j));
-                    }
-                }
-
-                world.setMyScreenPosition(positionUser);
-            }
-        };
-    }
-
-
-
     public WallSetup getSetup() {
-        return _set;
+        return _setup;
     }
 
 }
