72fe96464b99efe9fd1facc0d4519dd665daa169
Jean-Baptiste GOMOND <dev@jbgomond.com>
2018-01-19 00:31:26 +0100

    Corrections du merge précédent

--- app/src/main/java/com/ensicaen/ecole/ludistreet/RA/ARImageView.java
+++ app/src/main/java/com/ensicaen/ecole/ludistreet/RA/ARImageView.java
@@ -2,10 +2,6 @@ package com.ensicaen.ecole.ludistreet.RA;
 
 import android.content.Context;
 
-import com.ensicaen.ecole.ludistreet.model.WallModel;
-
-import gl.Color;
-
 /**
  * Created by jorand on 18/01/2018.
  */
