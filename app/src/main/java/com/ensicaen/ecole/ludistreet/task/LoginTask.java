a9aeb8456073149866b6314f5d93bcb26545137f
Jordan Ernult <you@example.com>
2018-01-18 19:01:07 +0100

    swaggy flow

--- app/src/main/java/com/ensicaen/ecole/ludistreet/task/LoginTask.java
+++ app/src/main/java/com/ensicaen/ecole/ludistreet/task/LoginTask.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import com.ensicaen.ecole.ludistreet.ChoiceActivity;
 import com.ensicaen.ecole.ludistreet.ModeActivity;
 import com.ensicaen.ecole.ludistreet.RA.ARView;
 import com.ensicaen.ecole.ludistreet.SelectWallActivity;
@@ -42,11 +43,8 @@ public class LoginTask extends AsyncTask<LoginModel, Void , Boolean> {
     protected void onPostExecute(Boolean result){
         if(result) {
 
-            Intent intent = new Intent(_activity, SelectWallActivity.class);
+            Intent intent = new Intent(_activity, ChoiceActivity.class);
             _activity.startActivity(intent);
-            // WallModel w = new WallModel(2,2);
-            // ARView arv = new ARView(w);
-            // ArActivity.startWithSetup(_activity, arv.getSetup());
             return;
         }
         Toast.makeText(_activity, "Erreur lors de la connexion",
